7d25e0a0a834a975269af769a6868c6bdd9082d0
Nicklas Israelsson <nicklas.israelsson@medius.com>
2024-07-11 16:53:42 +0200

    Add gallery dialog web component
    To get a dialog to browse images.

---
+++ src/image-dialog.js
@@ -0,0 +1,86 @@
+export default class extends HTMLElement {
+  constructor() {
+    super();
+    this.images = Array.from(this.querySelectorAll("picture"));
+    this.dialogImages = this.images.map((imgNode) => imgNode.cloneNode(true));
+    this.dialog = document.createElement("dialog");
+
+    this.closeBtn = this.createIconButton(this.getCloseIcon(), "Stäng dialog");
+    this.closeBtn.setAttribute("formmethod", "dialog");
+    let form = document.createElement("form");
+    form.appendChild(this.closeBtn);
+    this.dialog.appendChild(form);
+
+    this.prevBtn = this.createIconButton(this.getPrevIcon(), "Föregående bild");
+    this.prevBtn.setAttribute("index-modifier", "-1");
+    this.prevBtn.addEventListener("click", this);
+    this.dialog.appendChild(this.prevBtn);
+    this.dialogImages.forEach((image) => {
+      image.setAttribute("hidden", "");
+      this.dialog.appendChild(image);
+    });
+    this.nextBtn = this.createIconButton(this.getNextIcon(), "Nästa bild");
+    this.nextBtn.setAttribute("index-modifier", "1");
+    this.nextBtn.addEventListener("click", this);
+    this.dialog.appendChild(this.nextBtn);
+
+    this.appendChild(this.dialog);
+
+    this.images.forEach((img) => {
+      img.setAttribute("clickable", "");
+      img.addEventListener("click", this);
+    });
+
+    this.dialog.addEventListener("close", this);
+  }
+
+  createIconButton(icon, label) {
+    let btn = document.createElement("button");
+    btn.innerHTML = icon;
+    btn.setAttribute("aria-label", label);
+    return btn;
+  }
+
+  handleEvent(event) {
+    this[`on${event.type}`](event);
+  }
+
+  onclick(event) {
+    let modifier = event.currentTarget.getAttribute("index-modifier");
+    if (!modifier) {
+      let clickedImageIndex = this.images.indexOf(event.currentTarget);
+      this.dialogImages[clickedImageIndex].removeAttribute("hidden");
+      this.dialog.showModal();
+      return;
+    }
+    this.goToNextImage(modifier);
+  }
+
+  goToNextImage(modifier) {
+    let currentImageIndex = this.dialogImages.findIndex(
+      (image) => !image.hasAttribute("hidden")
+    );
+    let newIndex = currentImageIndex + +modifier;
+    if (!isFinite(newIndex)) return;
+    if (newIndex < 0) newIndex = this.dialogImages.length - 1;
+    if (newIndex >= this.dialogImages.length) newIndex = 0;
+    this.dialogImages[currentImageIndex].setAttribute("hidden", "");
+    this.dialogImages[newIndex].removeAttribute("hidden");
+  }
+
+  onclose() {
+    this.dialogImages.forEach((image) => image.setAttribute("hidden", ""));
+  }
+
+  getCloseIcon() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>`;
+  }
+
+  getNextIcon() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>`;
+  }
+
+  getPrevIcon() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>`;
+  }
+}
---
+++ src/show-hide.js
@@ -0,0 +1,79 @@
+export default class extends HTMLElement {
+  /**
+   * Instantiate the Web Component
+   */
+  constructor() {
+    // Get parent class properties
+    super();
+
+    // Get the elements
+    this.trigger = this.querySelector("[trigger]");
+    this.content = this.querySelector("[content]");
+    if (!this.trigger || !this.content) return;
+
+    // Setup default UI
+    this.trigger.removeAttribute("hidden");
+    this.trigger.setAttribute("aria-expanded", false);
+    this.content.setAttribute("hidden", "");
+
+    // Create the event handler
+    this.handler = this.createHandler();
+
+    document.addEventListener("click", this);
+  }
+
+  handleEvent(event) {
+    if (
+      event.target === this.trigger ||
+      event.composedPath().includes(this.trigger)
+    )
+      return;
+
+    if (this.trigger.getAttribute("aria-expanded") === "true") {
+      this.hide();
+    }
+  }
+
+  /**
+   * Create the event handler
+   */
+  createHandler() {
+    return (event) => {
+      // Don't let the button trigger other actions
+      event.preventDefault();
+
+      // If the content is expanded, hide it
+      // Otherwise, show it
+      if (this.trigger.getAttribute("aria-expanded") === "true") {
+        this.hide();
+      } else {
+        this.show();
+      }
+    };
+  }
+
+  show() {
+    this.trigger.setAttribute("aria-expanded", true);
+    this.content.removeAttribute("hidden");
+  }
+
+  hide() {
+    this.trigger.setAttribute("aria-expanded", false);
+    this.content.setAttribute("hidden", "");
+  }
+
+  /**
+   * Start listening to clicks
+   */
+  connectedCallback() {
+    if (!this.trigger || !this.content) return;
+    this.trigger.addEventListener("click", this.handler);
+  }
+
+  /**
+   * Stop listening to clicks
+   */
+  disconnectedCallback() {
+    this.trigger.removeEventListener("click", this.handler);
+  }
+}
