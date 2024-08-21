b8a7fe43318cf0ec9960070c87e951d2118c1adb
Nicklas Israelsson <nicklas.israelsson@medius.com>
2024-08-21 15:21:07 +0200

    Remove 'unsafe-inline' from CSP

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,5 +1,11 @@
 import showHide from "./show-hide.js";
 import imageDialog from "./image-dialog.js";
 
+document.querySelectorAll("[data-preload-google]").forEach((l) =>
+  l.addEventListener("load", (e) => {
+    e.target.rel = "stylesheet";
+  })
+);
+
 customElements.define("show-hide", showHide);
 customElements.define("image-dialog", imageDialog);
